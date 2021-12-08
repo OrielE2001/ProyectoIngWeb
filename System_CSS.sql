6c55c50996ca6335f2d24cf24c84c3e25dc4adcf
Yulissa Restrepo <yulissa.restrepo@utp.ac.pa>
2021-12-08 03:15:16 -0500

    modificacion de la creacion de la base de datos 1.5

--- System_CSS.sql
+++ System_CSS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-12-2021 a las 03:46:12
+-- Tiempo de generación: 08-12-2021 a las 09:09:56
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.11
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `systemcss`
 --
+CREATE DATABASE IF NOT EXISTS `systemcss` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `systemcss`;
 
 -- --------------------------------------------------------
 
@@ -29,13 +31,36 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cita` (
   `IDcita` char(5) NOT NULL,
-  `Fecha` date NOT NULL,
-  `Hora` time NOT NULL,
+  `Fecha_Hora` datetime NOT NULL,
   `IDpoliclinica_cita` char(5) NOT NULL,
   `IDdoctor_cita` char(5) NOT NULL,
   `IDpaciente_cita` char(5) NOT NULL,
-  `IDespecialidad_cita` char(5) NOT NULL
+  `IDespecialidad_cita` char(5) NOT NULL,
+  `Diagnostico` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- RELACIONES PARA LA TABLA `cita`:
+--   `IDdoctor_cita`
+--       `doctor` -> `IDdoctor`
+--   `IDespecialidad_cita`
+--       `especialidad` -> `IDespecialidad`
+--   `IDpaciente_cita`
+--       `paciente` -> `IDpaciente`
+--   `IDpoliclinica_cita`
+--       `policlinica` -> `IDpoliclinica`
+--
+
+--
+-- Disparadores `cita`
+--
+DELIMITER $$
+CREATE TRIGGER `ActualizarEstado` BEFORE UPDATE ON `cita` FOR EACH ROW UPDATE historial
+JOIN cita ON historial.IDcita_historial = cita.IDcita
+SET historial.Estado = 'ATENDIDO'
+WHERE historial.IDcita_historial = cita.IDcita
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -54,6 +79,14 @@ CREATE TABLE `doctor` (
   `IDpoliclinica_doctor` char(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- RELACIONES PARA LA TABLA `doctor`:
+--   `IDEspecialidad_doctor`
+--       `especialidad` -> `IDespecialidad`
+--   `IDpoliclinica_doctor`
+--       `policlinica` -> `IDpoliclinica`
+--
+
 -- --------------------------------------------------------
 
 --
@@ -65,6 +98,10 @@ CREATE TABLE `especialidad` (
   `Nombre` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- RELACIONES PARA LA TABLA `especialidad`:
+--
+
 -- --------------------------------------------------------
 
 --
@@ -72,12 +109,22 @@ CREATE TABLE `especialidad` (
 --
 
 CREATE TABLE `historial` (
-  `Codigo` char(5) NOT NULL,
-  `Estado` varchar(20) DEFAULT 'DISPONIBLE',
+  `Codigo` int(5) NOT NULL,
+  `Estado` varchar(15) DEFAULT 'CONFIRMADO',
   `IDusuario_historial` char(5) NOT NULL,
   `IDpaciente_historial` char(5) NOT NULL,
   `IDcita_historial` char(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- RELACIONES PARA LA TABLA `historial`:
+--   `IDcita_historial`
+--       `cita` -> `IDcita`
+--   `IDpaciente_historial`
+--       `paciente` -> `IDpaciente`
+--   `IDusuario_historial`
+--       `usuario` -> `IDusuario`
+--
 
 -- --------------------------------------------------------
 
@@ -89,11 +136,14 @@ CREATE TABLE `paciente` (
   `IDpaciente` char(5) NOT NULL,
   `Nombre` varchar(25) NOT NULL,
   `Cedula` varchar(13) NOT NULL,
-  `Correo` text NOT NULL,
   `Telefono` int(8) DEFAULT NULL,
   `Edad` int(3) NOT NULL,
   `Asegurado` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- RELACIONES PARA LA TABLA `paciente`:
+--
 
 -- --------------------------------------------------------
 
@@ -109,6 +159,10 @@ CREATE TABLE `policlinica` (
   `Horario` time(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- RELACIONES PARA LA TABLA `policlinica`:
+--
+
 -- --------------------------------------------------------
 
 --
@@ -121,6 +175,56 @@ CREATE TABLE `usuario` (
   `Correo` text NOT NULL,
   `Contraseña` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- RELACIONES PARA LA TABLA `usuario`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `view_historialcita`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `view_historialcita` (
+`NOMBRE` varchar(25)
+,`FECHA` datetime
+,`ASUNTO` text
+,`DOCTOR` varchar(25)
+,`ESTADO` varchar(15)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `view_listapaciente`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `view_listapaciente` (
+`#` char(5)
+,`paciente` varchar(25)
+,`especialidad` text
+,`Fecha_Hora` datetime
+,`Diagnostico` text
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `view_historialcita`
+--
+DROP TABLE IF EXISTS `view_historialcita`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_historialcita`  AS SELECT `paciente`.`Nombre` AS `NOMBRE`, `cita`.`Fecha_Hora` AS `FECHA`, `especialidad`.`Nombre` AS `ASUNTO`, `doctor`.`Nombre` AS `DOCTOR`, `historial`.`Estado` AS `ESTADO` FROM ((((`paciente` left join `cita` on(`cita`.`IDpaciente_cita` = `paciente`.`IDpaciente`)) left join `especialidad` on(`cita`.`IDespecialidad_cita` = `especialidad`.`IDespecialidad`)) left join `doctor` on(`cita`.`IDdoctor_cita` = `doctor`.`IDdoctor`)) left join `historial` on(`historial`.`IDcita_historial` = `cita`.`IDcita`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `view_listapaciente`
+--
+DROP TABLE IF EXISTS `view_listapaciente`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_listapaciente`  AS SELECT `cita`.`IDcita` AS `#`, `paciente`.`Nombre` AS `paciente`, `especialidad`.`Nombre` AS `especialidad`, `cita`.`Fecha_Hora` AS `Fecha_Hora`, `cita`.`Diagnostico` AS `Diagnostico` FROM ((`cita` left join `paciente` on(`cita`.`IDpaciente_cita` = `paciente`.`IDpaciente`)) left join `especialidad` on(`cita`.`IDespecialidad_cita` = `especialidad`.`IDespecialidad`)) ;
 
 --
 -- Índices para tablas volcadas
@@ -176,6 +280,16 @@ ALTER TABLE `policlinica`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`IDusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `historial`
+--
+ALTER TABLE `historial`
+  MODIFY `Codigo` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
 
 --
 -- Restricciones para tablas volcadas
